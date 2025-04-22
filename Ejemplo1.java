1a31956a0ffa25c12fa7dbe6b54c52217777e015
imartor99 <imartor769@g.educaand.es>
2025-04-22 07:59:05 +0000

    hago ejemplo de prueba

---
+++ Ejemplo1.java
@@ -0,0 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Ejemplo1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] array = {4,2,6,7};
+        int n;
+        boolean repetir = false;
+        do {
+            try {
+                repetir = false;
+                System.out.print("Introduce un número entero > 0 y <" + array.length + " " );
+                n = sc.nextInt();
+                System.out.println("Valor en la posición " + n + ":" + array[n]);
+            } 
+            catch (InputMismatchException e) {
+                sc.nextLine();
+                n = 0;
+                System.out.println("Debe introducir un número entero ");
+                repetir = true;
+            } 
+            catch (IndexOutOfBoundsException e){
+                System.out.println("Debe introducir un número enetero > 0 y < " + array.length + " ");
+                repetir = true;
+            }
+           
+        } while (repetir);
+
+    }
+}
