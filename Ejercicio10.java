69be08d4374bb33ae31a36e035334f483a7c502d
imartor99 <imartor769@g.educaand.es>
2025-04-24 11:00:31 +0000

    REALIZO EJERCICIO 10

---
+++ Ejercicio10.java
@@ -0,0 +1,29 @@
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        try {
+            System.out.println(metodo());
+        } catch (Exception e) {
+            System.out.println("Excepción en método() ");
+            e.printStackTrace();
+        }
+    }
+
+    public static int metodo() {
+        int valor = 0;
+        try {
+            valor = valor + 1;
+            valor = valor + Integer.parseInt("a");
+            valor = valor + 1;
+            System.out.println("Valor al final del try: " + valor);
+        } catch (NumberFormatException e) {
+            valor = valor + Integer.parseInt("42");
+            System.out.println("Valor al final del catch: " + valor);
+        } finally {
+            valor = valor + 1;
+            System.out.println("Valor al final del finally: " + valor);
+        }
+        valor = valor + 1;
+        System.out.println("Valor antes del return: " + valor);
+        return valor;
+    }
+}
