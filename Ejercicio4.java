b46758b779d3f31dd0ca8a5b77622c1c3882fe71
imartor99 <imartor769@g.educaand.es>
2025-04-28 07:22:43 +0000

    realizo ej7

---
+++ Ejercicio4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Introduce una cadena de texto: ");
+        String leeTeclado = entrada.nextLine();
+
+        try{
+            char caracter = Ejercicio3.caracterEn(leeTeclado, 7);
+            System.out.println("El caracter de la posición solicitada es: " + caracter);
+        }catch(Exception e){
+            System.out.println("Has intentado obtener un caracter de la cadena que no existe");
+        }
+
+        entrada.close();
+    }
+}
